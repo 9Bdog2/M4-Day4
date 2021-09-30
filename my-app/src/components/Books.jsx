f0cb87d83beb36ebb40547f19e056b354ab88ae6
Bogdan Birau <birau.bogdan92@gmail.com>
2021-09-30 16:59:29 +0100

    updated

--- my-app/src/components/Books.jsx
+++ my-app/src/components/Books.jsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import fantasy from "../data/fantasy.json";
-import Card from "react-bootstrap/Card"
+import Card from "react-bootstrap/Card";
+import CommentList from "./CommentList";
+
+class Books extends Component {
+  state = {
+    comments: [],
+  };
+
+  render() {
+    return (
+      <div className="d-flex flex-wrap">
+        {fantasy.map((book) => {
+          /* return <img className="book-cover" alt="" key={book.asin} src={book.img}/> */
+          return (
+            <Card className="book-cover">
+              <Card.Img variant="top" key={book.asin} src={book.img} />
+              <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <Card.Text>Price : {book.price}£</Card.Text>
+                <Card.Text>Category : {book.category}</Card.Text>
+                <CommentList comments={this.state.comments}/>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    );
+  }
+
+  componentDidMount = async () => {
+      const resp = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + this.props.book.asin,
+            {
+             headers: {  Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMWRlMDRiYjUzZDAwMTViMTllZDEiLCJpYXQiOjE2MzIzMTI4MDAsImV4cCI6MTYzMzUyMjQwMH0.ZEiTPl6hBe2UEm5T239Bdd_a6OldaCdb8ShgjSK-vsI"}
+             })
+
+             const retrivedComments = await resp.json()
+             this.setState({
+                 comments : retrivedComments
+             })
+  }
 
 
-export default function Books() {
-  console.log(fantasy);
-
-  return (
-    <div className= "d-flex flex-wrap">
-      {fantasy.map((book) => {
-        /* return <img className="book-cover" alt="" key={book.asin} src={book.img}/> */
-        return <Card className="book-cover">
-          <Card.Img variant="top" key={book.asin} src={book.img}/>
-          <Card.Body>
-            <Card.Title>{book.title}</Card.Title>
-          </Card.Body>
-        </Card>;
-      })}
-    </div>
-  );
 }
+
+export default Books;
